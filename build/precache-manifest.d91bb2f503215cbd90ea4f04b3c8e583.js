6be375f32eebde5c8cba2a5d14b8ce717d713e17
Angela Mu <angela@Angelas-MacBook-Pro-3.local>
2023-11-03 18:43:20 -0400

    first commit

---
+++ build/precache-manifest.d91bb2f503215cbd90ea4f04b3c8e583.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c80459acd2e4c7777a453922bafe41c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "804f0ac0d984f16b6ca4",
+    "url": "/static/js/2.8ed588f0.chunk.js"
+  },
+  {
+    "revision": "73081d93a0095c3a105b",
+    "url": "/static/js/main.1a9bdd32.chunk.js"
+  },
+  {
+    "revision": "c415adb34cc05b8216ce",
+    "url": "/static/js/runtime-main.81c4a991.js"
+  }
+]);
